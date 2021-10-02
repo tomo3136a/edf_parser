096bcbed0c17e6a4e2f2b4850773ef89138fc805
tomo3136a <tomo3136a@gmail.com>
2021-10-03 05:46:29 +0900

    modified parts_figure

--- parts_figure.ps1
+++ parts_figure.ps1
@@ -1,118 +1,104 @@
+#!powershell
 $path = "parts_list.csv"
-#$out = "parts_out.xlsx"
+
+function gen_pin($app, $n, $x1, $y1, $x2, $y2, $px, $py, $pw) {
+    if ($n -gt 0) {
+        $grp = @()
+        for ($i = 0; $i -lt $n; $i++) {
+            $x = (1.0 + 2 * $i) / (2 * $n) * ($x2 - $x1) + $x1 + 100
+            $y = (1.0 + 2 * $i) / (2 * $n) * ($y2 - $y1) + $y1 + 100
+            $app.ActiveSheet.Shapes.AddConnector(1, $x, $y, $x + $px, $y + $py).Select()
+            $grp += $app.Selection.ShapeRange.Name
+        }
+        if ($n -gt 1) {
+            $app.ActiveSheet.Shapes.Range($grp).Group().Select()
+        }
+        $app.Selection.ShapeRange.Line.ForeColor.RGB = [int]0x808080
+        $app.Selection.ShapeRange.Line.Weight = $pw
+        $app.Selection.ShapeRange.Name
+    }
+}
 
 function gen_figure($path) {
     $src = (Get-Location).path + "\" + $path
     $src = Resolve-Path $src
     $out = $src -replace "\.[^.]+$", "_out.xlsx"
-    "src: ${src}" | Out-Host
+    "in:  ${src}" | Out-Host
     "out: ${out}" | Out-Host
 
     $col = Import-Csv $src -Delimiter ","
-    
     try {
         $app = New-Object -ComObject Excel.Application
-        $app.Visible = $True
-        #$app.Visible = $False
+        #$app.Visible = $True
+        $app.Visible = $False
         #$app.DisplayAlerts = $False
+        $app.ScreenUpdating = $False
+        $app.EnableEvents = $False
+        "start generate" | Out-Host
         $book = $app.Workbooks.Add()
-        $sheet = $app.Worksheets.Item(1)
-        $sheet.Name = $sheet.Name -replace "sheet", "`zu"
+        $app.Calculation = $False
         # ---
-        $sheet.Cells.ColumnWidth = 1 * (72 / 2.54 / 10) + 1.3   # 10mm
-        $sheet.Cells.RowHeight = 1 * 72 / 2.54 + 0.5            # 1cm
-        $pos = 0
+        $book.Worksheets.Item(1).Select()
+        $app.ActiveSheet.Name = $app.ActiveSheet.Name -replace "sheet", ("" + [char]0x56f3)
+        $app.ActiveSheet.Cells.ColumnWidth = 1 * (72 / 2.54 / 10) + 1.3   # 10mm
+        $app.ActiveSheet.Cells.RowHeight = 1 * 72 / 2.54 + 0.5            # 1cm
+        # ---
+        $pos = 1
         $col | ForEach-Object {
             $line = $_
-            $w = (1.0 * [int]($line.width)) / 10 * 72 / 2.54
-            $h = (1.0 * [int]($line.height)) / 10 * 72 / 2.54
-            $fs = 11
+            $w = [float]$line.width / 10 * 72 / 2.54
+            $h = [float]$line.height / 10 * 72 / 2.54
+            $s = $line.text
 
-            $r = (1.0 * [int]($line.r)) + 5
-            $n1 = (1.0 * [int]($line.n1))
-            $n2 = (1.0 * [int]($line.n2))
-            $n3 = (1.0 * [int]($line.n3))
-            $n4 = (1.0 * [int]($line.n4))
-            $weight = 6
+            $r = [float]$line.r
+            $n1 = [int]$line.n1
+            $n2 = [int]$line.n2
+            $n3 = [int]$line.n3
+            $n4 = [int]$line.n4
 
-            $x = (1.0 * [int]($line.x + 10)) / 10 * 72 / 2.54
-            $y = (1.0 * [int]($line.y + 10)) / 10 * 72 / 2.54
-            $a = (1.0 * [int]($line.angle))
+            $x = ([float]$line.x + 10) / 10 * 72 / 2.54
+            $y = ([float]$line.y + 10) / 10 * 72 / 2.54
+            $a = [int]$line.angle % 360
 
-            "${w},${h},${fs}  ${r},${n1},${n2},${n3},${n4},${weight}  ${x},${y},${a}" | Out-Host
-            $shape = $sheet.Shapes.AddShape(1, $x, $y, $w, $h)
-            $shape.Rotation = $a
-            $shape.Placement = 3
-            $shape.LockAspectRatio = -1
-            $shape.TextFrame2.AutoSize = 0
-            $shape.TextFrame2.WordWrap = 0
-            $shape.TextFrame2.VerticalAnchor = 3
-            $shape.TextFrame2.HorizontalAnchor = 2
-            $shape.TextFrame2.MarginLeft = 2.83
-            $shape.TextFrame2.MarginRight = 2.83
-            $shape.TextFrame2.MarginTop = 2.83
-            $shape.TextFrame2.MarginBottom = 2.83
-            $shape.TextFrame2.TextRange.Text = $line.text
-            $shape.Fill.ForeColor.RGB = [int]($line.color)
-            $shape.TextFrame.HorizontalOverflow = 0
+            "${pos}: ${s}" | Out-Host
+            $app.ActiveSheet.Shapes.AddShape(1, ($x + 100), ($y + 100), $w, $h).Select()
+            $app.Selection.ShapeRange.TextFrame2.TextRange.Text = $s
+            $app.Selection.ShapeRange.TextFrame2.TextRange.Font.Fill.ForeColor.RGB = [int]0
+            $app.Selection.ShapeRange.TextFrame.HorizontalOverflow = 0
+            $app.Selection.ShapeRange.Fill.ForeColor.RGB = [int]$line.color
             if ([int]($line.font) -ne 0) {
-                $fs = [int]($line.font)
+                $app.Selection.ShapeRange.TextFrame2.TextRange.Font.Size = [int]$line.font
             }
-            $shape.TextFrame2.TextRange.Font.Size = $fs
-            $grp = @($shape.Name)
-            if ($n1 -gt 0) {
-                $d = $w / (2 * $n1)
-                for ($i = 0; $i -lt $n1; $i++) {
-                    $j = 2 * $i + 1
-                    $shape = $sheet.Shapes.AddConnector(1, $x, $y + $d * $j, $x - $r, $y + $d * $j)
-                    $shape.Line.Weight = $weight
-                    $grp += $shape.Name
-                }
+            if ((($n1 + $n2 + $n3 + $n4) * $r) -gt 0) {
+                $grp = @($app.Selection.ShapeRange.Name)
+                $grp += gen_pin $app $n1 $x $y $x ($y + $h) (-$r) 0 6
+                $grp += gen_pin $app $n2 $x ($y + $h) ($x + $w) ($y + $h) 0 $r 6
+                $grp += gen_pin $app $n3 ($x + $w) ($y + $h) ($x + $w) $y $r 0 6
+                $grp += gen_pin $app $n4 ($x + $w) $y $x $y 0 (-$r) 6
+                $app.ActiveSheet.Shapes.Range($grp).Group().Select()
             }
-            if ($n2 -gt 0) {
-                $d = $h / (2 * $n2)
-                for ($i = 0; $i -lt $n2; $i++) {
-                    $j = 2 * $i + 1
-                    $shape = $sheet.Shapes.AddConnector(1, $x + $d * $j, $y + $h, $x + $d * $j, $y + $h + $r)
-                    $shape.Line.Weight = $weight
-                    $grp += $shape.Name
-                }
-            }
-            if ($n3 -gt 0) {
-                $d = $w / (2 * $n3)
-                for ($i = 0; $i -lt $n3; $i++) {
-                    $j = 2 * $i + 1
-                    $shape = $sheet.Shapes.AddConnector(1, $x + $w, $y + $h - $d * $j, $x + $w + $r, $y + $h - $d * $j)
-                    $shape.Line.Weight = $weight
-                    $grp += $shape.Name
-                }
-            }
-            if ($n4 -gt 0) {
-                $d = $h / (2 * $n4)
-                for ($i = 0; $i -lt $n4; $i++) {
-                    $j = 2 * $i + 1
-                    $shape = $sheet.Shapes.AddConnector(1, $x + $w - $d * $j, $y, $x + $w - $d * $j, $y - $r)
-                    $shape.Line.Weight = $weight
-                    $grp += $shape.Name
-                }
-            }
-            $sheet.Shapes.Range($grp).Group() | Out-Null
+            $app.Selection.ShapeRange.TextFrame2.AutoSize = 0
+            $app.Selection.ShapeRange.TextFrame2.WordWrap = 0
+            $app.Selection.ShapeRange.TextFrame2.VerticalAnchor = 3
+            $app.Selection.ShapeRange.TextFrame2.HorizontalAnchor = 2
+            $app.Selection.ShapeRange.TextFrame2.MarginLeft = 2.83
+            $app.Selection.ShapeRange.TextFrame2.MarginRight = 2.83
+            $app.Selection.ShapeRange.TextFrame2.MarginTop = 2.83
+            $app.Selection.ShapeRange.TextFrame2.MarginBottom = 2.83
+            $app.Selection.ShapeRange.LockAspectRatio = -1
+            if ($a) { $app.Selection.ShapeRange.Rotation = $a }
+            $app.Selection.Placement = 3
             $pos ++
+            # $name = $app.Selection.ShapeRange.Name
+            # "name: ${name}" | Out-Host
         }
         # ---
         $book.SaveAs($out, 51)
+        "end generate" | Out-Host
     }
     catch {
     }
     finally {
-        if ($shape) {
-            [System.Runtime.InteropServices.Marshal]::ReleaseComObject($shape) | Out-Null
-            $shape = $null
-        }
-        if ($sheet) {
-            [System.Runtime.InteropServices.Marshal]::ReleaseComObject($sheet) | Out-Null
-            $sheet = $null
-        }
         if ($book) {
             [System.Runtime.InteropServices.Marshal]::ReleaseComObject($book) | Out-Null
             $book = $null
